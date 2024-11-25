78f1a6dc336b3e92eb49d14f5dac32fa561f191a
tushar-baghel <tusharbaghel704@gmail.com>
2024-11-25 21:48:06 +0530

    adding switch case code

---
+++ Java Practice/switchCase.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class switchCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        switch(num){
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("monday");
+                break;
+            case 3:
+                System.out.println("monday");
+                break;
+            case 4:
+                System.out.println("monday");
+                break;
+            case 5:
+                System.out.println("monday");
+                break;
+            case 6:
+                System.out.println("monday");
+                break;
+            case 7:
+                System.out.println("monday");
+                break;
+            default:
+                System.out.println("Please enter the number between 1 to 7");
+
+
+        }
+        sc.close();
+    }
+    
+}
