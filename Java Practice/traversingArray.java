23fa322631123c8e652123f48f84b568790c7446
tushar-baghel <tusharbaghel704@gmail.com>
2025-01-05 15:23:33 +0530

    Traversing an array and printing sum

--- Java Practice/traversingArray.java
+++ Java Practice/traversingArray.java
@@ -1,10 +1,19 @@
 public class traversingArray {
     public static void main(String[] args) {
         int num[] = {1,2,3,4,5};
-        for(int i = 0;i<num.length;i++){
-            
+        int sum = 0;
+        int target = 9;
+        for(int i = 0;i<num.length;i++){           
+            //int sum =0;
+                   
+            sum = sum + num[i];
             System.out.println(num[i]);
+            
+            
         }
+        System.out.println(sum);
+        
+        
 
     }
     
