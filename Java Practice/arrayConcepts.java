e808041040b71bc08d558c72f1e0c089692a3c79
tushar-baghel <tusharbaghel704@gmail.com>
2025-01-06 22:35:43 +0530

    Printing array using Array.toString

--- Java Practice/arrayConcepts.java
+++ Java Practice/arrayConcepts.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class arrayConcepts {
     public static void main(String[] args) {
-        int arr[] = new int[10]; // declaring and size
+        int arr[] = new int[5]; // declaring and size
         Scanner sc = new Scanner(System.in);
         for(int i = 0 ; i < arr.length ; i++){
             arr[i] = sc.nextInt(); // taking input through loop
         }
+        System.out.println(Arrays.toString(arr)); // printing an array
     }
     
 }
