c7907b2150ee038599b7244aa27b158639564fc1
tushar-baghel <tusharbaghel704@gmail.com>
2025-01-08 22:13:30 +0530

    initialized and took input for 2d array

--- Java Practice/arrayConcepts.java
+++ Java Practice/arrayConcepts.java
@@ -12,6 +12,16 @@ public class arrayConcepts {
         for(int i = 0 ; i < arr.length ; i++){
             System.out.println(arr[i]); // traversing each element of the array
         }
+        // 2d Array
+        int arr2[][] = new int[3][3]; // initialising 2d Array
+        for(int i = 0 ; i < arr2.length ; i++){
+            for(int j = 0 ; i < arr2.length ; j++){
+                arr2 [i][j] = sc.nextInt(); // taking input in 2d array
+            }
+        }
+        
+
+
     }
     
 }
