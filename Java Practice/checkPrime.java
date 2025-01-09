9135b5368686e51a912991075ef2d75d74f88c1c
tushar-baghel <tusharbaghel704@gmail.com>
2025-01-09 21:59:18 +0530

    Check prime logic added

--- Java Practice/checkPrime.java
+++ Java Practice/checkPrime.java
@@ -4,6 +4,11 @@ public class checkPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if(n%n == 0 && n%1 == 0){
+            System.out.println("Number is prime");
+        }else{
+            System.out.println("Number is not prime");
+        }
         
 
     
