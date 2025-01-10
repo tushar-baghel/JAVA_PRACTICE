9c9dcd03aaf07c76441138f1e7c69b223ba3952a
tushar-baghel <tusharbaghel704@gmail.com>
2025-01-10 22:03:07 +0530

    Brute force for logic

--- Java Practice/checkPrime.java
+++ Java Practice/checkPrime.java
@@ -4,10 +4,14 @@ public class checkPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if(n%n == 0 && n%1 == 0){
-            System.out.println("Number is prime");
-        }else{
-            System.out.println("Number is not prime");
+        for(int i = 2 ; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                System.out.println("Number is Not prime");
+                
+            }else{
+                System.out.println("Number is prime");
+                break;
+            }
         }
         
 
