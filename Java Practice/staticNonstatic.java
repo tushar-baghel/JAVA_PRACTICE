a2a76a469ccfafb2ef8740144ff94f6035c1dded
tushar-baghel <tusharbaghel704@gmail.com>
2025-05-06 21:51:53 +0530

    Adding static and non static practice code comments

--- Java Practice/staticNonstatic.java
+++ Java Practice/staticNonstatic.java
@@ -2,9 +2,19 @@ public class staticNonstatic {
     static {
         System.out.println("Radha Krishna");
     }
+    void newMethod2(){
+        System.out.println("Inside non static method");
+    }
     public static void main(String[] args) {
         System.out.println("Inside Main");
+        staticNonstatic.newMethod();
+        staticNonstatic obj = new staticNonstatic(); // creating object to invoke non static method
+        obj.newMethod2(); // calling non static method
         
+    }
+
+    static void newMethod(){
+        System.out.println("Inside static method");
     }
     
 }
