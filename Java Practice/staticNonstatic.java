7962f171192ce4d0882aeb3dc8d46b5f94b20a7c
tushar-baghel <tusharbaghel704@gmail.com>
2025-05-06 21:46:57 +0530

    Adding static and non static practice code

---
+++ Java Practice/staticNonstatic.java
@@ -0,0 +1,10 @@
+public class staticNonstatic {
+    static {
+        System.out.println("Radha Krishna");
+    }
+    public static void main(String[] args) {
+        System.out.println("Inside Main");
+        
+    }
+    
+}
